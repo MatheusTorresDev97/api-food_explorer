11a73a71ccbaea198481507555d001d31ddb35dc
Matheus <matheustorresdev@gmail.com>
2023-12-02 20:15:03 -0300

    feat/create database

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,5 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UsersControllers {
     async create(request, response) {
+      const { name, email, password } = request.body;
+
+      if (!email) {
+        throw new AppError("Email não foi informado!");
+      }
+      
       return response.status(201).json();
     }
   }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
+const AppError = require("./utils/AppError");
 
 const app = express();
 
@@ -9,5 +10,21 @@ app.use(express.json());
 app.use(routes);
 
 const PORT = 3333;
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.json({
+      status: error.statusError,
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return response.json({
+    status: 501,
+    message: "Erro interno do servidor.",
+  });
+});
 
 app.listen(PORT, () => console.log("Server is running on PORT:", PORT));
