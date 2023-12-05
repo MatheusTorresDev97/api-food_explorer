f9f5449f043f928f69a4c09420fea1963a1e0384
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-05 09:18:50 -0300

    feat/ adds the functionality to show information

--- src/services/order/OrderShowService.js
+++ src/services/order/OrderShowService.js
@@ -1,19 +1,22 @@
 class OrderShowService {
-    constructor(orderRepository) {
-      this.orderRepository = orderRepository;
-    }
-  
-    async execute(order_id) {
-      const order = await this.orderRepository.findById(order_id);
-  
-      const mealsOfThisOrder = await this.orderRepository.findMealsOfAnOrder(
-        order_id
-      );
-  
-      console.log({ order, mealsOfThisOrder });
-  
-      return order;
-    }
+  constructor(orderRepository) {
+    this.orderRepository = orderRepository;
   }
-  
-  module.exports = OrderShowService;
+
+  async execute(order_id) {
+    const order = await this.orderRepository.findById(order_id);
+
+    const mealsOfThisOrder = await this.orderRepository.findMealsOfAnOrder(
+      order_id
+    );
+
+    const orderWithMeals = {
+      ...order,
+      meals: [...mealsOfThisOrder],
+    };
+
+    return orderWithMeals;
+  }
+}
+
+module.exports = OrderShowService;
